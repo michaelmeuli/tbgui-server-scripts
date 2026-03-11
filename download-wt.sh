928deccd7b99d7eba6c5073ebb7bfcb7415455cd
Michael Meuli <mmeuli@imm.uzh.ch>
2026-03-11 20:40:22 +0100

    download-wt.sh

---
+++ download-wt.sh
@@ -0,0 +1,6 @@
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/001/SRR1200811/SRR1200811_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/001/SRR1200811/SRR1200811_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/008/SRR1200798/SRR1200798_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/007/SRR1200797/SRR1200797_2.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/008/SRR1200798/SRR1200798_1.fastq.gz
+wget -nc ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR120/007/SRR1200797/SRR1200797_1.fastq.gz
